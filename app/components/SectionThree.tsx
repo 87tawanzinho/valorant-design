25ffd5c23b67fa95615e4358ea30cd6bfa31facf
87tawanzinho <87tawanzinho@gmail.com>
2023-12-19 05:22:20 +0000

    new changes to banner like cursor

--- app/components/SectionThree.tsx
+++ app/components/SectionThree.tsx
@@ -1,8 +1,16 @@
 export default function SectionThree() {
   return (
     <div className="flexingBanner relative ">
-      <div className="w-2/4 bg-white h-80 rounded img-banner1 shadowBanner z-40"></div>
-      <div className="w-2/4 bg-white h-80 rounded img-banner2 shadowBanner z-40"></div>
+      <div className="w-2/4 bg-white h-80 rounded img-banner1 shadowBanner z-40 flex items-end justify-start cursor-pointer">
+        <p className=" text-xl z-60 bg-black">
+          ATUALIZAÇÃO de ANTICHEAT // INVERNO 2023
+        </p>
+      </div>
+      <div className="w-2/4 bg-white h-80 rounded img-banner2 shadowBanner z-40 flex items-end justify-start cursor-pointer">
+        <p className="text-xl z-60 bg-black">
+          Notas da Atualização 7.12 do VALORANT
+        </p>
+      </div>
     </div>
   );
 }
