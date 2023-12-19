81d378e35d0b66eacfd2b55bb29503c498cf3b42
87tawanzinho <87tawanzinho@gmail.com>
2023-12-19 04:57:28 +0000

    finish banner

---
+++ app/components/SectionThree.tsx
@@ -0,0 +1,8 @@
+export default function SectionThree() {
+  return (
+    <div className="flexingBanner relative ">
+      <div className="w-2/4 bg-white h-80 rounded img-banner1 shadowBanner z-40"></div>
+      <div className="w-2/4 bg-white h-80 rounded img-banner2 shadowBanner z-40"></div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,27 @@ import NavBar from "./components/Navbar";
 import SectionOne from "./components/SectionOne";
 import ImageComponent from "./components/ImageComponent";
 import SectionTwo from "./components/SectionTwo";
+import SectionThree from "./components/SectionThree";
 
 export default function Home() {
   return (
     <main className="flex justify-center flex-col items-center h-screen">
-      <div className=" width-container bg-valorant relative overflow-hidden">
+      <div className=" width-container bg-valorant relative overflow-y-scroll">
         <ImageComponent />
 
         <NavBar />
         <div className="padding-sections">
           <SectionOne />
           <SectionTwo />
-          <p className="text-xs text-center p-2 w-80 absolute  bottom-0 left-0 opacity-30">
-            Este projeto foi desenvolvido para simular o cliente do Valorant. a
-            sua concepção foi voltada para computadores, assim como o cliente
-            original. Para uma experiência visual única, assegure-se de estar
-            utilizando um computador.
-          </p>
+          <SectionThree />
+          <div className="text-center">
+            <p className="text-xs text-center p-2 w-80   opacity-30">
+              Este projeto foi desenvolvido para simular o cliente do Valorant.
+              a sua concepção foi voltada para computadores, assim como o
+              cliente original. Para uma experiência visual única, assegure-se
+              de estar utilizando um computador.
+            </p>
+          </div>
         </div>
       </div>
     </main>
